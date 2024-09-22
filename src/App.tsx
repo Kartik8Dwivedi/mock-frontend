917087629d17ddf2bc030788e08801208c7dd064
Kartik <codewithme.kartik@gmail.com>
2024-09-22 17:06:03 +0530

    added logic and frontend to capture and show the user's video on the frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
 
 function App() {
 
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+const getUserVideo = () => {
+    const video = navigator.mediaDevices.getUserMedia({ video: true });
+
+    video.then((stream) => {
+      const videoElement = document.createElement("video");
+      videoElement.srcObject = stream;
+      videoElement.play();
+      // appending this element to the element with class name as add-video
+      document.querySelector(".add-video")?.appendChild(videoElement);
+      // document.body.appendChild(videoElement);
+    });
+}
+
+const handleAuthentication = () =>{
+  getUserVideo();
+  return (document?.getElementById("my_modal_1") as HTMLDialogElement)?.showModal();
+}
+  
+  return (
+    <div className="flex w-screen h-screen justify-center items-center">
+      <button
+        className="btn btn-secondary"
+        onClick={handleAuthentication}
+      >
+        Authenticate
+      </button>
+      <dialog id="my_modal_1" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Facial Authentication</h3>
+          <p className="py-4 add-video">
+            Press ESC key or click the button below to close
+          </p>
+          <div className="modal-action">
+            <form method="dialog">
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+    </div>
+  );
 }
 
 export default App
